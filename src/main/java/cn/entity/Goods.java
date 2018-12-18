6a78df3a57602d97fc495ea4f8514e0a682e2b1e
xiaoming275 <806446877@qq.com>
2018-12-18 13:39:27 +0800

    啊啊啊

---
+++ src/main/java/cn/entity/Goods.java
@@ -0,0 +1,40 @@
+package cn.entity;
+
+public class Goods {
+	private int id;
+	private String name;
+	private int price;
+	private String descr;
+	private String image;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescr() {
+		return descr;
+	}
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+}
---
+++ src/main/java/cn/goods/dao/GoodsDao.java
@@ -0,0 +1,10 @@
+package cn.goods.dao;
+
+import java.util.List;
+
+import cn.entity.Goods;
+
+public interface GoodsDao {
+
+	public List<Goods> findgoods();
+}
